bebd2b08397ff611d46e2e6ac8898737d4d08867
Eiphodos <david.hagerman.olzon@gmail.com>
2022-03-29 18:34:24 +0200

    Added a variant that uses the official MONAI UNETR implementation for the decoder but the timm ViT implementation.

--- models/model_builder.py
+++ models/model_builder.py
@@ -33,4 +33,30 @@ def build_model(cfg):
             res_block=True,
             dropout_rate=0.0,
         )
+    elif cfg.model == 'UNETR_OfficialCustom':
+        encoder = ViTMAE(
+                        vol_size=cfg.vol_size,
+                        patch_size=cfg.patch_size,
+                        in_chans=cfg.in_chans,
+                        input_dim=cfg.input_dim,
+                        qkv_bias=cfg.qkv_bias,
+                        use_abs_pos_emb=cfg.abs_pos_emb,
+                        use_rel_pos_bias=cfg.rel_pos_bias#,
+                        #out_indices=[3, 7, 11]
+                         )
+        encoder.init_weights(cfg.pretrained)
+        model = UNETR(
+            encoder,
+            in_channels=cfg.in_chans,
+            out_channels=cfg.output_dim,
+            img_size=cfg.vol_size,
+            feature_size=16,
+            hidden_size=768,
+            mlp_dim=3072,
+            num_heads=12,
+            pos_embed="perceptron",
+            norm_name="instance",
+            res_block=True,
+            dropout_rate=0.0,
+        )
     return model
