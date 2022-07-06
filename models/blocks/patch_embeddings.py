ba85e479b78035797fe75e97d67c9f3f97b4b651
Eiphodos <david.hagerman.olzon@gmail.com>
2022-07-06 17:54:29 +0200

    Trying out a deeper patch embedding.

--- models/blocks/patch_embeddings.py
+++ models/blocks/patch_embeddings.py
@@ -2,6 +2,7 @@ from typing import Sequence, Type, Union
 
 import torch.nn as nn
 import torch.nn.functional as F
+from monai.networks.blocks.unetr_block import UnetrBasicBlock
 from monai.networks.layers import Conv
 from monai.utils import ensure_tuple_rep
 from torch.nn import LayerNorm
@@ -129,18 +130,10 @@ class PatchEmbed3D(nn.Module):
 
         return x
 
-class PatchEmbed3D(nn.Module):
-    """ Volume to Patch Embedding.
-
-    Args:
-        patch_size (int): Patch token size. Default: (2,2,2).
-        in_chans (int): Number of input volume channels. Default: 1.
-        embed_dim (int): Number of linear projection output channels. Default: 48.
-        norm_layer (nn.Module, optional): Normalization layer. Default: None
-    """
-
-    def __init__(self, vol_size=(96, 96, 96), patch_size=(2, 2, 2), in_chans=1, embed_dim=48, norm_layer=None):
+class PatchEmbedDeep(nn.Module):
+    def __init__(self, vol_size=(96, 96, 96), patch_size=(2, 2, 2), in_chans=1, embed_dim=48, spatial_dims=3, norm_name="instance", norm_layer=None):
         super().__init__()
+
         patches_resolution = [vol_size[0] // patch_size[0], vol_size[1] // patch_size[1], vol_size[2] // patch_size[2]]
         self.vol_size = vol_size
         self.patch_size = patch_size
@@ -150,7 +143,27 @@ class PatchEmbed3D(nn.Module):
         self.in_chans = in_chans
         self.embed_dim = embed_dim
 
+        self.block1 = UnetrBasicBlock(
+            spatial_dims=spatial_dims,
+            in_channels=in_chans,
+            out_channels=embed_dim,
+            kernel_size=3,
+            stride=1,
+            norm_name=norm_name,
+            res_block=True,
+        )
+        self.block2 = UnetrBasicBlock(
+            spatial_dims=spatial_dims,
+            in_channels=in_chans,
+            out_channels=embed_dim,
+            kernel_size=3,
+            stride=1,
+            norm_name=norm_name,
+            res_block=True,
+        )
+
         self.proj = nn.Conv3d(in_chans, embed_dim, kernel_size=patch_size, stride=patch_size)
+
         if norm_layer is not None:
             self.norm = norm_layer(embed_dim)
         else:
@@ -167,7 +180,12 @@ class PatchEmbed3D(nn.Module):
         if D % self.patch_size[0] != 0:
             x = F.pad(x, (0, 0, 0, 0, 0, self.patch_size[0] - D % self.patch_size[0]))
 
+        # Creating patch embedding
+        x = self.block1(x)
+        x = self.block2(x)
         x = self.proj(x)  # B C D Wh Ww
+
+        # Normalization if flag set
         if self.norm is not None:
             D, Wh, Ww = x.size(2), x.size(3), x.size(4)
             x = x.flatten(2).transpose(1, 2).contiguous()
@@ -175,7 +193,6 @@ class PatchEmbed3D(nn.Module):
             x = x.transpose(1, 2).contiguous().view(-1, self.embed_dim, D, Wh, Ww)
 
         return x
-
 
 class PatchEmbedLarge(nn.Module):
 
