f89da10365858bbe1a04d68b95f41e585b77dd3f
David <david.hagerman.olzon@gmail.com>
2022-07-27 12:26:18 +0200

    Fixed so that global token is repeated different amount of times depending on the layer/block (since resolution changes)

--- models/backbones/swin_nnformer.py
+++ models/backbones/swin_nnformer.py
@@ -135,7 +135,10 @@ class WindowAttention(nn.Module):
             N += 1
 
         if global_token is not None:
-            x = torch.cat((x, global_token), dim=1)
+            batch_size = global_token.size(0)
+            gbt = torch.cat(tuple([global_token[i].repeat(self.n_windows, 1, 1) for i in range(batch_size)]),
+                            dim=0)  # (B_, 1, C)
+            x = torch.cat((x, gbt), dim=1)
             N += 1
 
         qkv = self.qkv(x)
@@ -561,10 +564,7 @@ class SwinTransformerNNFormer(nn.Module):
         dpr = [x.item() for x in torch.linspace(0, drop_path_rate, sum(depths))]  # stochastic depth decay rule
 
         if global_token:
-            self.n_windows = ceil(self.pretrain_img_size[0] / window_size[0]) * ceil(
-                self.pretrain_img_size[1] / window_size[0]) * \
-                        ceil(self.pretrain_img_size[2] / window_size[0])
-            self.global_token = nn.Parameter(torch.zeros(1, 1, embed_dim))
+            self.global_token = nn.Parameter(torch.zeros(1, 1, 1, embed_dim))
             trunc_normal_(self.global_token, std=.02)
 
         # build layers
@@ -628,8 +628,7 @@ class SwinTransformerNNFormer(nn.Module):
 
         if self.global_token is not None:
             batch_size = x.size(0)
-            B_ = batch_size * self.n_windows
-            global_token = self.global_token.expand(B_, -1, -1)
+            global_token = self.global_token.expand(batch_size, -1, -1, -1)
         else:
             global_token = None
 
