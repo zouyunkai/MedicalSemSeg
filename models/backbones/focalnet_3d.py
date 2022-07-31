2e1b56ae42f5123a116ae95cef15d713011f9c1f
Eiphodos <david.hagerman.olzon@gmail.com>
2022-07-31 21:24:38 +0200

    Increased value of epsilon and reduced init scale trying to improve stability.

--- models/backbones/focalnet_3d.py
+++ models/backbones/focalnet_3d.py
@@ -140,6 +140,7 @@ class FocalModulationBlock(nn.Module):
         mlp_hidden_dim = int(dim * mlp_ratio)
         self.mlp = Mlp(in_features=dim, hidden_features=mlp_hidden_dim, act_layer=act_layer, drop=drop)
 
+        self.S = None
         self.H = None
         self.W = None
 
